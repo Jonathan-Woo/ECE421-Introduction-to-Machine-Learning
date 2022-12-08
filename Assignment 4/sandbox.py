909a111e2078ce1b5457f74bce79c6301e2f9c85
Jonathan Woo <jonathan.ck.woo@gmail.com>
2022-12-07 22:26:49 -0500

    Finished assignments

--- Assignment 4/sandbox.py
+++ Assignment 4/sandbox.py
@@ -1,10 +1,38 @@
 #%%
 import numpy as np
+import matplotlib.pyplot as plt
+import tensorflow_datasets as tfds
 
-#%%
-a = [i for i in range(5)]
+train, test= tfds.load(
+    'rock_paper_scissors',
+    split=['train','test'],
+)
+
+train_numpy = np.vstack(tfds.as_numpy(train))
+test_numpy = np.vstack(tfds.as_numpy(test))
+
+train_numpy = train_numpy[:int(train_numpy.size*0.8)]
+test_numpy = test_numpy[:int(test_numpy.size*0.8)]
+
+X_train = np.array(list(map(lambda x: x[0]['image'], train_numpy)))
+Y_train = np.array(list(map(lambda x: x[0]['label'], train_numpy)))
+X_train = X_train.transpose(0, 3, 1, 2) / 255.0
+Y_train = Y_train.flatten()
+
+X_test = np.array(list(map(lambda x: x[0]['image'], test_numpy)))
+Y_test = np.array(list(map(lambda x: x[0]['label'], test_numpy)))
+X_test = X_test.transpose(0, 3, 1, 2) / 255.0
+Y_test = Y_test.flatten()
+
 # %%
-for i in a:
-    i += 1
-    print(i)
+import tensorflow as tf
+#.load_data() by default returns a split between training and test set. 
+# We then adjust the training set into a format that can be accepted by our CNN
+(X_train, Y_train), (X_test, Y_test) = tf.keras.datasets.cifar10.load_data()
+X_train = X_train.transpose(0, 3, 1, 2) / 255.0
+Y_train = Y_train.flatten()
+X_test = X_test.transpose(0, 3, 1, 2) / 255.0
+Y_test = Y_test.flatten()
+
+
 # %%
